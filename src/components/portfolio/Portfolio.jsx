89531d947b4c8595fbf5f860e6971a8ac889b8e2
Grace Akinremi <akinremiomoyisola@gmail.com>
2021-10-27 18:30:26 +0100

    Intro component created

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-
+            <h1>Portfolio</h1>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="" />
+                    <h3>Qeola App</h3>
+                </div>
+            </div>
         </div>
     )
 }
