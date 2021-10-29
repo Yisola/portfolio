7f1401cd707b6f257c39e2e92861dbcebc13e5b9
Grace Akinremi <akinremiomoyisola@gmail.com>
2021-10-29 14:06:03 +0100

    Created testimonial component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
+import Testimonial from "./components/testimonial/Testimonial"
 import Contact from "./components/contact/Contact";
 import "./app.scss";
 
@@ -20,6 +21,7 @@ const [menuOpen, setMenuOpen] = useState(false)
         <Intro />
         <Portfolio />
         <Works />
+        <Testimonial />
         <Contact />
       </div>
     </div>
